32d7ba76c18b63e7af19010865806b1d240a702f
Scott Liang <xingang_liang@outlook.com>
2020-02-06 19:04:39 +0800

    添加项目文件。

---
+++ MyConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 5;
+            b += a;
+            Console.WriteLine(b);
+            Console.ReadLine();
+        }
+    }
+}
